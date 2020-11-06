9578c74eeb64020e86523ce3ed125e975a957fc1
Gusluxi <gullegustav@gmail.com>
2020-11-06 14:53:47 +0100

    Making methods in TimeClass public for Javadoc web index.

--- src/com/pizza/TimeClass.java
+++ src/com/pizza/TimeClass.java
@@ -15,7 +15,7 @@ public class TimeClass {
     /**
      * @return a formatted String of the current time in (Hours:minutes)
      */
-    String displayTime(){
+    public String displayTime(){
         return currentRealTime;
     }
 
@@ -24,11 +24,11 @@ public class TimeClass {
     /**
      * Uses the localTime utility Java provides of only hours and minutes.
      * The method's parameters will be used to set a custom time.
-     * @param hours
-     * @param min
+     * @param hours     Input of hours for when order is ready.
+     * @param min       Input of minutes for when order is ready.
      * @return a String after hour-minute input
      */
-    String setTimeOfTheDay(int hours, int min) {
+    public String setTimeOfTheDay(int hours, int min) {
         LocalTime setTime = LocalTime.of(hours,min);
         String timeTest = setTime.toString();
         return timeTest;
@@ -40,9 +40,9 @@ public class TimeClass {
      * This method asks the user to insert a time attribute for an order.
      * The method uses input via UserInput class to assign valuse to hours and to
      * minutes as well as validating a set parameters and if it is an integer.
-     * @param order
+     * @param order     The object of the current ArrayList of order.
      */
-    void askForTime(Order order){
+    public void askForTime(Order order){
         //Ask for input, then assign value to timer
         int timeHourSat = UserInput.inputInt(0,23,"Skriv en bestemt time:");
         int timeMinSat = UserInput.inputInt(0,59,"Skriv en bestemt minute:");
