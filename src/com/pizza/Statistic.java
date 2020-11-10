c615eee33b7fe0c5a4ef3f86339f219ef2c7b661
Mick3460 <73465505+Mick3460@users.noreply.github.com>
2020-11-10 14:44:17 +0100

    vi har problemer med at sortere i vores HashMap

--- src/com/pizza/Statistic.java
+++ src/com/pizza/Statistic.java
@@ -9,28 +9,26 @@ public class Statistic {
 
 
     void readStats() throws FileNotFoundException {
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
 
         Scanner scanFile = new Scanner(new File(FILENAME));
         while (scanFile.hasNext()) {
             String word = scanFile.next();
-            if ( map.containsKey(word)) {
+            if (map.containsKey(word)) {
                 int count = map.get(word) + 1;
                 map.put(word, count);
             } else {
                 map.put(word, 1);
             }
         }
-        System.out.println(map);
-
 
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println(entry);
         }
     }
-
-
-
-
 }
+
+
+
+
